001419244683afdc8fa87b9006bf6d64b4411359
Harishanth THARMALINGAM <harishanth.tharmalingam@leoo.fr>
2020-06-19 01:33:40 +0200

    Added routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 // Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Home from './components/home';
 
 import './style.scss';
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/home" component={Home}>
+          <Home />
+        </Route>
+        <Route path="/:id" component={Movie}>
+          <Movie />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
